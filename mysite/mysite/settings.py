df20bcbbfea3bfcf864c41824bbb446a98d7a9f7
Li <sr.frontenddev210@gmail.com>
2019-06-10 04:44:26 +0800

    Generate Sqlite db, Update setting, Add gitignore file
    Signed-off-by: Li <sr.frontenddev210@gmail.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '61s-xmpi_*f-v%d&lx6jze828!a*)b_aiu$!s0&^4w9nn0vvho'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
